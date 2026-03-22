c1e48eab31e8db63609a3d29382dda4f59524bd7
DanBel <dan.bel.89@bk.ru>
2026-03-22 20:42:33 +0400

    web: base URL на опубликованный API

--- product-web/src/api.js
+++ product-web/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1',
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    'https://belkin-detailing-booking.matstart.ru/api/v1',
 })
 
 api.interceptors.request.use((config) => {
